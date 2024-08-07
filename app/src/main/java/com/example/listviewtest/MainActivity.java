388155272101b42586e5566201f5e2220eae78b5
D1204463 <161418099+D1204463@users.noreply.github.com>
2024-08-07 12:12:37 +0800

    透過客製化Adapter與樣式的xml的方式，讓ListView的內容能自動變化

--- app/src/main/java/com/example/listviewtest/MainActivity.java
+++ app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.listviewtest;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -9,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -22,6 +26,34 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private ListView citiesListView;
     private List<String> cities = new ArrayList<String>();
+
+    public class CityAdapter extends ArrayAdapter<City>{
+
+        public CityAdapter(@NonNull Context context, ArrayList<City>cities) {
+            super(context, 0, cities);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            // 找到哪一筆資料
+            City city = getItem(position);
+            if(convertView == null){
+                convertView = LayoutInflater.
+                        from(getContext()).
+                        inflate(R.layout.city_information,
+                                parent,
+                                false);
+            }
+            // 將資料塞入設計好的樣式
+            TextView tvName = (TextView) convertView.findViewById(R.id.cityname);
+            TextView tvZipCode = (TextView) convertView.findViewById(R.id.zipCode);
+
+            tvName.setText(city.name);
+            tvZipCode.setText(String.valueOf(city.zipCode));
+
+            return convertView;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +71,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     private void setCities(){
-        cities = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
-        cities.add("Hualien");
-        cities.add("Hsinchu");
-        cities.add("Changhua");
-        cities.add("Penghu");
-        //new ArrayAdapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,cities);
+//        cities = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
+//        cities.add("Hualien");
+//        cities.add("Hsinchu");
+//        cities.add("Changhua");
+//        cities.add("Penghu");
+//        //new ArrayAdapter
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,cities);
+//        citiesListView.setAdapter(adapter);
+        ArrayList<City> cityList = new ArrayList<City>();
+        cityList.add(new City("Taipei",100));
+        cityList.add(new City("New Taipei City",173));
+        cityList.add(new City("Taichung",407));
+        CityAdapter adapter = new CityAdapter(this,cityList);
         citiesListView.setAdapter(adapter);
     }
 
